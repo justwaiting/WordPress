b5767226ee2f7d48f63bbc6d6c7f7d48e92279f9
sumuth <sumuth@microsft.com>
2016-03-28 12:06:19 -0700

    wordpress for azure

---
+++ wp-config.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', getenv('DBNAME'));
+/** MySQL database username */
+define('DB_USER', getenv('DBUSER'));
+/** MySQL database password */
+define('DB_PASSWORD', getenv('DBPASSWORD'));
+/** MySQL hostname */
+define('DB_HOST', getenv('DBHOSTNAME'));
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+//Security key settings
+/** If you need to generate the string for security keys mentioned above, you can go the automatic generator to create new keys/values: https://api.wordpress.org/secret-key/1.1/salt **/
+define('AUTH_KEY',         getenv('authentication Key'));
+define('SECURE_AUTH_KEY',  getenv('secure Authentication Key'));
+define('LOGGED_IN_KEY',    getenv('logged In Key'));
+define('NONCE_KEY',        getenv('nonce Key'));
+define('AUTH_SALT',        getenv('authentication Salt'));
+define('SECURE_AUTH_SALT', getenv('secure Authentication Salt'));
+define('LOGGED_IN_SALT',   getenv('logged In Salt'));
+define('NONCE_SALT',       getenv('nonce Salt'));
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
